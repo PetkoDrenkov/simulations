ebc6641e07d71a49af72f2f4ab3e28347b2a9b00
PetkoDrenkov <petko727150@gmail.com>
2026-04-08 15:39:51 +0300

    Update bessel.py

--- laser_beams/bessel.py
+++ laser_beams/bessel.py
@@ -3,17 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from scipy.special import jv
 
-v = 0
+v = 1
 r_0 = 1
-r = np.linspace(0, 70, 5000)
+r = np.linspace(0, 30, 5000)
 p = np.linspace(0, 2*np.pi, 5000)
 R, P = np.meshgrid(r, p)
 
 Z = jv(v,R*r_0)
 X, Y = R*np.cos(P), R*np.sin(P)
+I = Z**2
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X, Y, Z,cmap=cm.cividis)
+ax.plot_surface(X, Y, I,cmap=cm.cividis)
 fig.savefig(f'bb_v={v}_r0={r_0}.png')
 plt.show()
