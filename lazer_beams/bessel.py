c184de449a258bc9a022119a691fe61142588187
PetkoDrenkov <petko727150@gmail.com>
2026-04-08 12:38:51 +0300

    Create bessel.py

---
+++ lazer_beams/bessel.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from scipy.special import jv
+
+v = 0
+r_0 = 1
+r = np.linspace(0, 70, 5000)
+p = np.linspace(0, 2*np.pi, 5000)
+R, P = np.meshgrid(r, p)
+
+Z = jv(v,R*r_0)
+X, Y = R*np.cos(P), R*np.sin(P)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(X, Y, Z,cmap=cm.cividis)
+fig.savefig(f'bb_v={v}_r0={r_0}.png')
+plt.show()
