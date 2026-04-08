01c9f1b512178b87129f9e960fef01ec1b93ecb9
PetkoDrenkov <petko727150@gmail.com>
2026-04-08 15:41:12 +0300

    Update bessel_gauss.py

--- laser_beams/bessel_gauss.py
+++ laser_beams/bessel_gauss.py
@@ -9,9 +9,9 @@ where v is topological charge (and order) of the beam (bessel function),
 r_0 is radius and w_0 is thickness of the beam's fourier image (a ring)
 '''
 
-r_0 = 8
+r_0 = 4
 w_0 = 0.2
-v = 0
+v = 1
 r = np.linspace(0., 30, 5000)
 p = np.linspace(0,2*np.pi,5000)
 R, P = np.meshgrid(r, p)
@@ -19,10 +19,11 @@ X, Y = R*np.cos(P), R*np.sin(P)
 
 N =  (w_0**2 / 2) * np.exp(1j*v*(P-np.pi/2))
 
-E_0 = N * np.exp(-(w_0*R/2)**2) * jv(v,R*r_0)
+E = N * np.exp(-(w_0*R/2)**2) * jv(v,R*r_0)
+I = np.abs(E)**2
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X, Y, E_0, cmap=cm.cividis)
-fig.savefig(f'bgb_v={v}_r0={r_0}_w0={w_0}.png')
+ax.plot_surface(X, Y, I, cmap=cm.cividis)
+# fig.savefig(f'bgb_v={v}_r0={r_0}_w0={w_0}.png')
 plt.show()
