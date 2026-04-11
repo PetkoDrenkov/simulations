11cdead72fb5bae52eda36cacbc7b511e707e794
PetkoDrenkov <petko727150@gmail.com>
2026-04-11 15:40:15 +0300

    Update gauss.py

--- laser_beams/gauss.py
+++ laser_beams/gauss.py
@@ -1,25 +1,27 @@
 import numpy as np
 """
-When setting inputs of the func, k and w_0 should be strictly positive!
-When using outputs of the func, they are ordered like in the return statement.
-gg = pure gaussian(2,900) 
-X = gg[0]
-Y = gg[1]
-I = gg[2]
-g_00 = gg[3]
-w_0 = gg[4]
+When setting inputs of the func, k and w_0 should be strictly positive.
+When using outputs of the func, they are ordered like in the return statement. For example:
+g = pure gaussian(0.002,5905249)
+grid: x = g[0]; y = g[1]
+intensity: i_gb = g[2]
+amplitude: g_00 = g[3]
+radius of the waist: w_0 = g[4]
+wavenumber: (k = 2*pi/ wavelength): k = g[5]
+z-axis: z = g[6]
+beam radius: w = g[7]
 """
 
 def pure_gaussian(w_0,k,z=0):
-    r = np.linspace(-5, 5, 100)
-    p = np.linspace(-5, 5, 100)
-    X, Y = np.meshgrid(r, p)
+    x = np.linspace(-5, 5, 100)
+    y = np.linspace(-5, 5, 100)
+    x, y = np.meshgrid(x, y)
     z_r = (k*w_0**2)/2
     w = w_0 * np.sqrt(1 + (z/z_r)**2)
-    N = (w_0/w) * np.exp(1j*(k*z + np.atan(z/z_r) +z*k*(X**2 + Y**2)/(z_r**2+z**2)))
+    ph = (w_0/w) * np.exp(1j*(k*z + np.atan(z/z_r) +z*k*(x**2 + y**2)/(z_r**2 + z**2)))
     
-    g_00 = N *  np.exp(-(X**2 + Y**2)/w**2) 
-            
-    I = np.abs(g_00)**2
+    g_00 = ph * np.exp(-(x**2 + y**2)/w**2)
+
+    i_gb = np.abs(g_00)**2
     
-    return X, Y, I, g_00, w_0, k, z
+    return x, y, i_gb, g_00, w_0, k, z, w
