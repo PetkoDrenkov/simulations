c8c5d99699f28c7a651e28469661f3242d3b5d3a
PetkoDrenkov <petko727150@gmail.com>
2026-04-11 11:25:06 +0300

    Update gauss.py

--- laser_beams/gauss.py
+++ laser_beams/gauss.py
@@ -1,18 +1,23 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
+"""
+When setting inputs of the func, k and w_0 should be strictly positive!
+When using outputs of the func, they are ordered like in the return statement.
+gg = pure gaussian(2,900) 
+X = gg[0]
+Y = gg[1]
+I = gg[2]
+g_00 = gg[3]
+w_0 = gg[4]
+"""
 
+def pure_gaussian(w_0,k,z=0):
+    r = np.linspace(-5, 5, 100)
+    p = np.linspace(-5, 5, 100)
+    X, Y = np.meshgrid(r, p)
+    z_r = (k*w_0**2)/2
+    w = w_0 * np.sqrt(1 + (z/z_r)**2)
 
-r = np.linspace(0, 2, 100)
-p = np.linspace(0, 2*np.pi, 1000)
-R, P = np.meshgrid(r, p)
-g_00 = np.exp(-R**2)
-
-X, Y = R*np.cos(P), R*np.sin(P)
-I = g_00**2
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X, Y, I,cmap=cm.cividis)
-fig.savefig('gb.png')
-plt.show()
+    g_00 = ((w_0/w) * np.exp(-(X**2 + Y**2)/w**2) *
+            np.exp(1j*(k*z + np.atan(z/z_r) +z*k*(X**2 + Y**2)/(z_r**2+z**2))))
+    I = np.abs(g_00)**2
+    return X, Y, I, g_00, w_0
